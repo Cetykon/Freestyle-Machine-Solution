3ae79274bba18ab5a3a7dc2cfd08cd843fd6a574
Jesus Aguilar <jesusaguilar92348@gmail.com>
2025-03-19 23:51:25 -0400

    Extracted FluidLevelReport Class

--- Coca-Cola Project/MainForm.cs
+++ Coca-Cola Project/MainForm.cs
@@ -145,32 +145,8 @@ namespace Coca_Cola_Project
         // Button to Fluid Level Report
         private void btnSyrupSt_Click(object sender, EventArgs e)
         {
-            string strLowlvlFluids = "";
-            string strSyrupSt = "";
-
-            for (int intCounter = 0; intCounter <= 9; intCounter++)
-            {
-                // Add all Flavors and their current amount of liquid to a string
-                strSyrupSt = strSyrupSt + flavors.StrSodaNames[intCounter] + ": " + flavors.dblSyrupBoxs[intCounter].ToString("N2") + "oz" + Constants.vbNewLine;
-
-                // Add fluids that are in a low threshold to a string
-                if (flavors.dblSyrupBoxs[intCounter] < 12d)
-                {
-                    strLowlvlFluids = strLowlvlFluids + flavors.StrSodaNames[intCounter] + ": " + flavors.dblSyrupBoxs[intCounter].ToString("N2") + "oz" + Constants.vbNewLine;
-                }
-                if (flavors.dblCo2Box < 54d)
-                {
-                    strLowlvlFluids = strLowlvlFluids + Constants.vbNewLine + "Co2: " + flavors.dblCo2Box.ToString("N2");
-                }
-            }
-
-            if (string.IsNullOrEmpty(strLowlvlFluids))
-            {
-                strLowlvlFluids = "No Fluid Found!";
-            }
-            strSyrupSt = strSyrupSt + Constants.vbNewLine + "Co2: " + flavors.dblCo2Box.ToString("N2") + "oz" + Constants.vbNewLine;
-
-            Interaction.MsgBox("Syrup and Co2 Stats:" + Constants.vbNewLine + Constants.vbNewLine + strSyrupSt + Constants.vbNewLine + "Fluids at low threshold:" + Constants.vbNewLine + Constants.vbNewLine + strLowlvlFluids + Constants.vbNewLine);
+            FluidLevelReport fluidLevelReport = new FluidLevelReport();
+            Interaction.MsgBox(fluidLevelReport.GetReport(flavors));
         }
         private void btnSodaSt_Click(object sender, EventArgs e)
         {
